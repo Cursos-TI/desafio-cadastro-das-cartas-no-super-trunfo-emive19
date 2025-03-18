9a7d2b3baefbef303d2db8c39d6fbde910442f06
emive19 <marcusviniciussfs@gmail.com>
2025-03-18 00:54:56 +0000

    carta 1

--- cartassupertrunfo.c
+++ cartassupertrunfo.c
@@ -2,4 +2,14 @@
 
 int main(){
     printf("desafio cartas super trunfo!\n");
+
+    //carta um
+    char estado;
+    char codigo[50];
+    char cidade[50];
+    int populaçao;
+    float area;
+    float PIB;
+    int turisticos;
+
 }
